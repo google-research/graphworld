3751ac0a31fa789d2ef8b2ec6a935c0aa46931d6
Brandon Mayer <bmayer@google.com>
2021-06-11 15:19:15 -0400

    Add more flexibility to config.

--- sampling/batch/src/generate_stochastic_block_model_with_features.py
+++ sampling/batch/src/generate_stochastic_block_model_with_features.py
@@ -17,8 +17,8 @@ from smb.smb_simulator import GenerateStochasticBlockModelWithFeatures
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_multi_string("gin_files", None, "Path to the config files.")
-flags.DEFINE_multi_string("gin_bindings", None, "Gin parameter bindings.")
+flags.DEFINE_multi_string('gin_files', None, 'Path to the config files.')
+flags.DEFINE_multi_string('gin_bindings', None, 'Gin parameter bindings.')
 
 @gin.configurable
 @dataclass
@@ -33,20 +33,20 @@ def main(argv):
     logging.info('sampler_config: %s', sampler_config)
 
     data = None
-    if sampler_config.generator_name == "StochasticBlockModel":
+    if sampler_config.generator_name == 'StochasticBlockModel':
         generator_config = sampler_config.generator_config
 
         data = GenerateStochasticBlockModelWithFeatures(
-            num_vertices = generator_config["num_verticies"],
-            num_edges = generator_config["num_edges"],
-            pi=np.array([0.25, 0.25, 0.25,0.25]),
-            prop_mat=np.ones((4, 4)) + 9.0 * np.diag(np.ones(4)),
-            feature_center_distance=2.0,
-            feature_dim=16,
-            edge_center_distance=2.0,
-            edge_feature_dim=4)
+            num_vertices = generator_config.get('num_verticies',10),
+            num_edges = generator_config.get('num_edges', 100),
+            pi = generator_config.get('pi', np.array([0.25, 0.25, 0.25, 0.25])),
+            prop_mat = generator_config.get('prop_mat', np.ones((4, 4)) + 9.0 * np.diag([1,1,1,1])),
+            feature_center_distance = generator_config.get('feaeture_center_distance', 2.0),
+            feature_dim = generator_config.get('feature_dim',16),
+            edge_center_distance = generator_config.get('edge_center_distance',2.0),
+            edge_feature_dim = generator_config.get('edge_feature_dim',4))
     else:
-        raise ValueError("Unknown Generator Name: {}".format(sampler_config.generator_name))
+        raise ValueError('Unknown Generator Name: {}'.format(sampler_config.generator_name))
 
 
     logging.info('%s', data)
