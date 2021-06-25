1a4915983a6147feb2c97721275a4d1f823eab5d
Brandon Mayer <bmayer@google.com>
2021-06-25 10:34:31 -0400

    Removing obsolete build worker script.

--- sampling/batch/build_worker_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Build and tag the research-graph-synthetic worker image.
-
-docker build -f ./Dockerfile.BeamWorker . -t research-graph-synthetic-worker
-docker tag research-graph-synthetic-worker gcr.io/research-graph/research-graph-synthetic-worker:latest
