7cbb5e7eaebb8575f2cf27d1ea80bdb300390431
Brandon Mayer <bmayer@google.com>
2021-07-10 17:50:01 -0400

    Add latest tag to image names when building locally.

--- beam/build_local.sh
+++ beam/build_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Build and tag the research-graph-synthic image.
 
-docker build . -t research-graph-synthetic -t gcr.io/research-graph/research-graph-synthetic
+docker build . -t research-graph-synthetic:latest -t gcr.io/research-graph/research-graph-synthetic:latest
