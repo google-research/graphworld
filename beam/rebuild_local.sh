274e01108bd4bc61c905c9d830e101866de00340
Brandon Mayer <bmayer@google.com>
2021-07-13 10:46:46 -0400

    Tag images as latest in rebuild_local.sh

--- beam/rebuild_local.sh
+++ beam/rebuild_local.sh
@@ -2,4 +2,4 @@
 # Build and tag the research-graph-synthic image.
 # Same as ./build_local.sh just don't use the cached layers and start from scratch.
 
-docker build --no-cache . -t research-graph-synthetic -t gcr.io/research-graph/research-graph-synthetic
+docker build --no-cache . -t research-graph-synthetic:latest -t gcr.io/research-graph/research-graph-synthetic:latest
