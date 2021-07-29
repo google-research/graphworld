6bc0dc596bb32e873bafac2d454872bda2f77ac9
John Palowitch <johnpalowitch@gmail.com>
2021-07-28 17:11:33 -0700

    Factor out BenchmarkGCN par do.

---
+++ beam/src/models/beam.py
@@ -0,0 +1,60 @@
+import logging
+
+import apache_beam as beam
+
+import models.wrappers
+
+class BenchmarkGNNParDo(beam.DoFn):
+  def __init__(self, output_path, num_features, num_classes, hidden_channels, epochs):
+    self._output_path = output_path
+    self._num_features = num_features
+    self._num_classes = num_classes
+    self._hidden_channels = hidden_channels
+    self._epochs = epochs
+
+  def process(self, element):
+    sample_id = element['sample_id']
+    torch_data = element['torch_data']
+    masks = element['masks']
+    skipped = element['skipped']
+
+    out = {
+      'skipped': skipped,
+      'results': None
+    }
+
+    if skipped:
+      logging.info(f'Skipping benchmark for sample id {sample_id}')
+      return
+
+    train_mask, val_mask, test_mask = masks
+    linear_model = wrappers.LinearGCN(
+      self._num_features,
+      self._num_classes,
+      self._hidden_channels,
+      train_mask,
+      val_mask,
+      test_mask)
+
+    losses = linear_model.train(self._epochs, torch_data)
+    test_accuracy = None
+    try:
+      # Divide by zero somesimtes happens with the ksample masks.
+      test_accuracy = linear_model.test(torch_data)
+    except:
+      logging.info(f'Failed to compute test accuracy for sample id {sample_id}')
+
+    results = {
+      'sample_id': sample_id,
+      'losses': losses,
+      'test_accuracy': test_accuracy,
+      'generator_config': element['generator_config']
+    }
+
+    results_object_name = os.path.join(self._output_path, '{0:05}_results.txt'.format(sample_id))
+    with beam.io.filesystems.FileSystems.create(results_object_name, 'text/plain') as f:
+      buf = bytes(json.dumps(results), 'utf-8')
+      f.write(buf)
+      f.close()
+
+    yield results
---
+++ beam/src/models/wrappers.py
@@ -0,0 +1,55 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+
+import models
+
+
+class LinearGCN():
+  def __init__(self, num_features, num_classes, hidden_channels, train_mask, val_mask, test_mask,
+               generator_name):
+    self._model = models.LinearGCNModel(num_features, num_classes, hidden_channels)
+    self._optimizer = torch.optim.Adam(self._model.parameters(), lr=0.01, weight_decay=5e-4)
+    self._criterion = torch.nn.CrossEntropyLoss()
+    self._train_mask = train_mask
+    self._val_mask = val_mask
+    self._test_mask = val_mask
+    self._generator_name = generator_name
+
+  def train_step(self, data):
+    self._model.train()
+    self._optimizer.zero_grad()  # Clear gradients.
+    out = self._model(data.x, data.edge_index)  # Perform a single forward pass.
+    loss = self._criterion(out[self._train_mask],
+                           data.y[self._train_mask])  # Compute the loss solely based on the training nodes.
+    loss.backward()  # Derive gradients.
+    self._optimizer.step()  # Update parameters based on gradients.
+    return loss
+
+  def test(self, data):
+    self._model.eval()
+    out = self._model(data.x, data.edge_index)
+    pred = out.argmax(dim=1)  # Use the class with highest probability.
+    test_correct = pred[self._test_mask] == data.y[self._test_mask]  # Check against ground-truth labels.
+    test_acc = int(test_correct.sum()) / int(self._test_mask.sum())  # Derive ratio of correct predictions.
+    return test_acc
+
+  def train(self, epochs, data):
+    losses = {}
+    for epoch in range(epochs):
+      loss = self.train_step(data)
+      losses['{0:05}'.format(epoch + 1)] = '{0}'.format(loss)
+
+    return losses
