a39cf1b5ce86ea8c02e03862c52707b7f0f18846
John Palowitch <johnpalowitch@gmail.com>
2021-12-30 22:34:15 -0800

    Remove unused model/benchmarker classes.:

--- src/graph_world/models/models.py
+++ src/graph_world/models/models.py
@@ -18,45 +18,6 @@ import torch.nn.functional as F
 from torch_geometric.nn import GATConv, GCNConv, global_mean_pool
 
 
-class GCNNodeModel(torch.nn.Module):
-  def __init__(self, num_features, num_classes, hidden_channels):
-    super(GCNNodeModel, self).__init__()
-    torch.manual_seed(12345)
-    self.conv1 = GCNConv(num_features, hidden_channels)
-    self.conv2 = GCNConv(hidden_channels, num_classes)
-
-  def forward(self, x, edge_index):
-    x = self.conv1(x, edge_index)
-    x = x.relu()
-    x = F.dropout(x, p=0.5, training=self.training)
-    x = self.conv2(x, edge_index)
-    return x
-
-
-class GCNGraphModel(torch.nn.Module):
-  def __init__(self, num_features, hidden_channels):
-    super(GCNGraphModel, self).__init__()
-    torch.manual_seed(12345)
-    self.conv1 = GCNConv(num_features, hidden_channels)
-    self.conv2 = GCNConv(hidden_channels, hidden_channels)
-    self.lin = Linear(hidden_channels, 1)
-
-  def forward(self, x, edge_index, batch):
-    # 1. Obtain node embeddings
-    x = self.conv1(x, edge_index)
-    x = x.relu()
-    x = self.conv2(x, edge_index)
-
-    # 2. Readout layer
-    x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
-
-    # 3. Apply a final classifier
-    x = F.dropout(x, p=0.5, training=self.training)
-    x = self.lin(x)
-
-    return x
-
-
 class PyGBasicGraphModel(torch.nn.Module):
   """This model wraps all basic_gnn models with a global pooling layer,
   turning them into graph models."""
