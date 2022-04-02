4cd6293e643f6bd49f30c69616ebcd3271733562
John Palowitch <johnpalowitch@gmail.com>
2022-04-01 18:11:36 -0700

    Add license headers to py files.

--- src/graph_world/sbm/sbm_simulator.py
+++ src/graph_world/sbm/sbm_simulator.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# @title Internal code for SBM generator class
-# pasted from https://github.com/google-research/google-research/blob/master/graph_embedding/simulations/sbm_simulator.py
 import collections
 import enum
 import math
